fe74e10f5ee134f6f14fac8900f6fc795962e313
Wmsdoc <2932226128@qq.com>
2023-03-15 20:19:13 +0800

    first test for thymeleaf

--- src/main/java/com/etc/test/controller/LoginController.java
+++ src/main/java/com/etc/test/controller/LoginController.java
@@ -12,10 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     @Resource
     UserService userService;
-    @RequestMapping(value = "hello",method = RequestMethod.GET)
-    public String hello(){
-        return "hello";
-    }
     @RequestMapping(value = "login",method = RequestMethod.GET)
     public String dologin(@RequestParam("userName") String userName,@RequestParam("password") String password){
 
